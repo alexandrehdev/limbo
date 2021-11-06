0c0f53618460c2fb83098f106fb6dcf1973f94d6
GuiLeoni14 <guileone167@gmail.com>
2021-11-06 21:04:10 -0200

    adicinando card de erro

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-const btn = document.querySelector(".title-name");
-btn.addEventListener("click", function () {
-	window.location.href = "index.php";
-});
+// const btn = document.querySelector(".title-name");
+// btn.addEventListener("click", function () {
+// 	window.location.href = "index.php";
+// });
 
-document.querySelector('.btn-account').addEventListener('click',function(){
-	window.location.href = "../pages/registerLogin/index.php";
-});
+// document.querySelector('.btn-account').addEventListener('click',function(){
+// 	window.location.href = "../pages/registerLogin/index.php";
+// });
 
+const erro = document.getElementById('msgerro');
+const login = document.getElementById('login');
 
+function testeMsgErr(){
+	erro.classList.toggle('active');
+	$('#msgerro > span').remove();
+	$('#msgerro').append(
+		'<span>' + 'aoba' + '</span>'
+	);
+	console.log('adicionando');
+}
+
+login.addEventListener('click', testeMsgErr)
