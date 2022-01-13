e0ea63a62e4aeb02a029474a557866f5554b8390
hls <hallyssonrdev@gmail.com>
2022-01-12 21:52:28 -0300

    login system on the way

--- src/Controller/Dump.php
+++ src/Controller/Dump.php
@@ -5,15 +5,13 @@ class Dump
 {
 	private $insertQuery;
 	
-	private $selectQuery;
+	
 
 	function __construct(){
 		$this->setInsertQuery(
 			"INSERT INTO limbo.userdata (username,email,password,cpassword) 
-					VALUES (:username,:email,:password,:cpassword)
+			VALUES (:username,:email,:password,:cpassword)
 			");
-
-		$this->setSelectQuery("SELECT * FROM limbo.userdata");
 	}
 
 	public function getInsertQuery(){
@@ -24,13 +22,12 @@ class Dump
 		$this->insertQuery = $query;
 	}
 
-	public function getSelectQuery(){
-		return $this->selectQuery;
+	public function selectQuery($user,$pwd){
+		$sql = "SELECT username,password FROM limbo.userdata WHERE username = '$user' AND password = '$pwd'";
+		
+		return $sql;
 	}
 
-	public function setSelectQuery($query){
-		$this->selectQuery = $query;
-	}
 	
 }
 
---
+++ src/Controller/GetDataLogin.php
@@ -0,0 +1,32 @@
+<?php 
+	namespace MyApp\Controller;
+
+	class GetDataLogin
+	{
+		private $email;
+
+		private $password;
+
+		function __construct()
+		{
+			$this->setMailLogin($_POST['log-mail']);
+			$this->setPasswordLogin($_POST['log-pass']);
+		}
+
+		public function getMailLogin(){
+			return $this->email;
+		}
+
+		public function setMailLogin($mail){
+			$this->email = $mail;
+		}
+
+		public function getPasswordLogin(){
+			return $this->password;
+		}
+
+		public function setPasswordLogin($pwd){
+			$this->password = $pwd;
+		}
+	}
+ ?>
