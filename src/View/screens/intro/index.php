cb76da4a6515953d1de37fbfb6f8e2ab70aa33b7
Hallysson Alexandre <hallyssonrdev@gmail.com>
2022-02-10 21:43:24 -0300

    almost done

--- src/View/screens/intro/index.php
+++ src/View/screens/intro/index.php
@@ -30,6 +30,20 @@
 		<div class="text middle" data-text="LIMBO">
 			LIMBO
 		</div>
+		<div class="mini-card-contents">
+			<div class="mini-card">
+				asd
+			</div>
+			<div class="mini-card">
+				asd
+			</div>
+			<div class="mini-card">
+				asd
+			</div>
+			<div class="mini-card">
+				asd
+			</div>
+		</div>
 		<div class="text-description">
 			<span>Gaming Club</span>
 		</div>
